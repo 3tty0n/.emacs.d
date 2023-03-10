450593835a75c3c7981d0e56542e0f9082185e08
Yusuke Izawa <me@yizawa.com>
2023-03-10 18:53:51 +0900

    Update my-font.el

--- site-lisp/my-font.el
+++ site-lisp/my-font.el
@@ -3,10 +3,21 @@
 ;;;
 ;;; Code:
 
-(if (string-equal system-type "gnu/linux")
-    (progn
-      (add-to-list 'default-frame-alist '(font . "Fira Code Medium-9"))
-      (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP"))))
+(defmacro with-system (type &rest body)
+  "Evaluate BODY if `system-type' equals TYPE."
+  (declare (indent defun))
+  `(when (eq system-type ',type)
+     ,@body))
+
+(with-system gnu/linux
+             (progn
+               (add-to-list 'default-frame-alist '(font . "Fira Code Medium-9"))
+               (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP")))
+             )
+
+(with-system darwin
+             (set-face-attribute 'default nil
+                                 :family "Fira Code" :height 130 :weight "Retina"))
 
 (provide 'my-font)
 ;;; my-font.el ends here
